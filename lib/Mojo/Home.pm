63f3ee767617681bd57a3d3cce0190619b56502b
Sebastian Riedel <sri@labs.kraih.com>
2009-03-19 06:43:51 +0100

    fixed small error in home detection

--- lib/Mojo/Home.pm
+++ lib/Mojo/Home.pm
@@ -55,16 +55,16 @@ sub detect {
             $path =~ s/$file$//;
             my @home = File::Spec->splitdir($path);
 
-            if (@home) {
-
-                # Remove "lib" and "blib"
-                pop @home while $home[-1] =~ /^b?lib$/ || $home[-1] eq '';
-
-                # Check for executable
-                return $self->parts(\@home)
-                  if -f File::Spec->catfile(@home, 'bin', $name)
-                      || -f File::Spec->catfile(@home, 'bin', 'mojo');
+            # Remove "lib" and "blib"
+            while (@home) {
+                last unless $home[-1] =~ /^b?lib$/ || $home[-1] eq '';
+                pop @home;
             }
+
+            # Check for executable
+            return $self->parts(\@home)
+              if -f File::Spec->catfile(@home, 'bin', $name)
+                  || -f File::Spec->catfile(@home, 'bin', 'mojo');
         }
     }
 

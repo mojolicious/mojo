b90b2c059a85dae671d0f272ee1b2f693bd26097
yuki-kimoto <kimoto.yuki@gmail.com>
2009-09-06 13:35:31 +0900

    fix module lib directory Mojolicious::Command::Generate::App

--- lib/Mojolicious/Command/Generate/App.pm
+++ lib/Mojolicious/Command/Generate/App.pm
@@ -82,8 +82,8 @@ use warnings;
 
 use FindBin;
 
-use lib "$FindBin::Bin/lib";
 use lib "$FindBin::Bin/../lib";
+use lib "$FindBin::Bin/../../lib";
 
 # Check if Mojo is installed
 eval 'use Mojolicious';

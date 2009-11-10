1d12ee5f7c5bb4956f2ea02c3ad4cef8fb013655
Sebastian Riedel <sri@labs.kraih.com>
2009-11-10 06:44:45 +0100

    fixed typos

--- lib/Mojo.pm
+++ lib/Mojo.pm
@@ -28,7 +28,7 @@ __PACKAGE__->attr(home   => sub { Mojo::Home->new });
 __PACKAGE__->attr(log    => sub { Mojo::Log->new });
 
 # Oh, so they have internet on computers now!
-our $VERSION = '0.999901';
+our $VERSION = '0.999902';
 
 sub new {
     my $self = shift->SUPER::new(@_);

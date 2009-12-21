993647a7211a10b98fcaf000e0318321c3a34b36
Sebastian Riedel <sri@labs.kraih.com>
2009-12-21 02:31:38 +0100

    updated example app

--- lib/Mojolicious/Command/Generate/App.pm
+++ lib/Mojolicious/Command/Generate/App.pm
@@ -101,8 +101,7 @@ sub startup {
     my $r = $self->routes;
 
     # Default route
-    $r->route('/:controller/:action/:id')
-      ->to(controller => 'example', action => 'welcome', id => 1);
+    $r->route('/:controller/:action/:id')->to('example#welcome', id => 1);
 }
 
 1;

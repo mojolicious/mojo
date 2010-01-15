9e3464f08bc87e1fb626561f44b6261761fed3f9
Sebastian Riedel <sri@labs.kraih.com>
2010-01-15 08:36:32 +0100

    updated lite_app example to make it work as new users would expect

--- lib/Mojolicious/Command/Generate/LiteApp.pm
+++ lib/Mojolicious/Command/Generate/LiteApp.pm
@@ -39,7 +39,7 @@ get '/' => 'index';
 
 get '/:groovy' => sub {
     my $self = shift;
-    $self->render_text($self->param('groovy'));
+    $self->render_text($self->param('groovy'), layout => 'funky');
 };
 
 shagadelic;

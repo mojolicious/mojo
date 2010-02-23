dcafd200c160f0ba13738342271a95ba248d2eb6
Sebastian Riedel <sri@labs.kraih.com>
2010-02-23 04:26:09 +0100

    made process id and lock file defaults more userfriendly in Mojo::Server::Daemon

--- lib/Mojo/Server/Daemon.pm
+++ lib/Mojo/Server/Daemon.pm
@@ -12,6 +12,7 @@ use Carp 'croak';
 use Fcntl ':flock';
 use File::Spec;
 use IO::File;
+use Mojo::Command;
 use Mojo::IOLoop;
 use Scalar::Util 'weaken';
 use Sys::Hostname 'hostname';
@@ -21,16 +22,18 @@ __PACKAGE__->attr(ioloop => sub { Mojo::IOLoop->singleton });
 __PACKAGE__->attr(keep_alive_timeout => 15);
 __PACKAGE__->attr(
     lock_file => sub {
+        my $self = shift;
         return File::Spec->catfile(File::Spec->splitdir(File::Spec->tmpdir),
-            'mojo_daemon.lock');
+            Mojo::Command->class_to_file(ref $self->app) . '.lock');
     }
 );
 __PACKAGE__->attr(max_clients             => 1000);
 __PACKAGE__->attr(max_keep_alive_requests => 100);
 __PACKAGE__->attr(
     pid_file => sub {
+        my $self = shift;
         return File::Spec->catfile(File::Spec->splitdir(File::Spec->tmpdir),
-            'mojo_daemon.pid');
+            Mojo::Command->class_to_file(ref $self->app) . '.pid');
     }
 );
 __PACKAGE__->attr(websocket_timeout => 300);

f073a0893d7a289ffef4b2d4b94c0020e41684d9
Sebastian Riedel <sri@labs.kraih.com>
2010-03-29 12:38:40 +0200

    fixed a listen bug

--- lib/Mojo/IOLoop.pm
+++ lib/Mojo/IOLoop.pm
@@ -273,7 +273,7 @@ sub listen {
     else {
 
         # Socket options
-        $options{LocalAddr} = $args->{address} || IPV6 ? '::' : '0.0.0.0';
+        $options{LocalAddr} = $args->{address} || (IPV6 ? '::' : '0.0.0.0');
         $options{LocalPort} = $args->{port}    || 3000;
         $options{Proto}     = 'tcp';
         $options{ReuseAddr} = 1;

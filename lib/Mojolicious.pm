cf15d0659052a6d802c997af6e340e3d4c163ff1
Sebastian Riedel <sri@cpan.org>
2011-04-20 14:14:03 +0200

    fixed changes

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -42,7 +42,7 @@ has static   => sub { Mojolicious::Static->new };
 has types    => sub { Mojolicious::Types->new };
 
 our $CODENAME = 'Smiling Cat Face With Heart-Shaped Eyes';
-our $VERSION  = '1.20';
+our $VERSION  = '1.21';
 
 # "These old doomsday devices are dangerously unstable.
 #  I'll rest easier not knowing where they are."

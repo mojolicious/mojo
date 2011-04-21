d9145abedbbebe226f9f6f3b22488de88809ba4d
Sebastian Riedel <sri@cpan.org>
2011-04-21 17:45:09 +0200

    added test

---
+++ t/mojolicious/lib/MojoliciousTest/Exceptional.pm
@@ -0,0 +1,18 @@
+package MojoliciousTest::Exceptional;
+use Mojo::Base 'Mojolicious::Controller';
+
+# "Dr. Zoidberg, can you note the time and declare the patient legally dead?
+#  Can I! That’s my specialty!"
+sub render_exception {
+  my ($self, $e) = @_;
+  $self->render_text("Action died: $e");
+}
+
+sub this_one_dies { die "doh!\n" }
+
+sub this_one_might_die {
+  die "double doh!\n" unless shift->req->headers->header('X-DoNotDie');
+  return 1;
+}
+
+1;

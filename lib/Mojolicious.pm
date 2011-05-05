468ae5ef50c722002d7e9a3070b1dc5649ac3f00
Sebastian Riedel <sri@cpan.org>
2011-05-05 22:43:25 +0200

    release preparation

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -41,7 +41,7 @@ has static   => sub { Mojolicious::Static->new };
 has types    => sub { Mojolicious::Types->new };
 
 our $CODENAME = 'Tropical Drink';
-our $VERSION  = '1.30';
+our $VERSION  = '1.3';
 
 # "These old doomsday devices are dangerously unstable.
 #  I'll rest easier not knowing where they are."

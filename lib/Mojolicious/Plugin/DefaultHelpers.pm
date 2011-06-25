e8303588acb742718374f90581d968d96bdd3bd0
Sebastian Riedel <sri@cpan.org>
2011-06-25 06:59:00 +0200

    added more url_for examples

--- lib/Mojolicious/Plugin/DefaultHelpers.pm
+++ lib/Mojolicious/Plugin/DefaultHelpers.pm
@@ -273,6 +273,12 @@ Page title.
 
 Generate a portable L<Mojo::URL> object with base for a route, path or URL.
 
+  %# Generate "/perldoc" if application is deployed under "/"
+  %= url_for '/perldoc'
+
+  %# Generate "/myapp/perldoc" if application is deployed under "/myapp"
+  %= url_for '/perldoc'
+
 =head1 METHODS
 
 L<Mojolicious::Plugin::DefaultHelpers> inherits all methods from

3ef9cbca9f771bd4b2afe2a6a04ec5342d0a5081
Sebastian Riedel <sri@cpan.org>
2011-06-28 15:18:40 +0200

    fixed example

--- lib/Mojolicious/Lite.pm
+++ lib/Mojolicious/Lite.pm
@@ -473,7 +473,8 @@ Restrictive placeholders can also be used for format detection.
   # /hello.txt
   get '/hello' => [format => [qw/json txt/]] => sub {
     my $self = shift;
-    return $self->render_json({hello => 'world!'});
+    return $self->render_json({hello => 'world!'})
+      if $self->stash('format') eq 'json';
     $self->render_text('hello world!');
   };
 

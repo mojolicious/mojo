8c56e7deaccccf2e874abcd5597b11b3f50c2a4c
Sebastian Riedel <sri@cpan.org>
2011-07-25 16:06:59 +0200

    made Mojo::Loader synopsis more foolproof

--- lib/Mojo/Loader.pm
+++ lib/Mojo/Loader.pm
@@ -78,9 +78,14 @@ Mojo::Loader - Loader
 
   use Mojo::Loader;
 
+  # Find modules in a namespace
   my $loader = Mojo::Loader->new;
-  my $modules = $loader->search('Some::Namespace');
-  $loader->load($modules->[0]);
+  for my $module (@{$loader->search('Some::Namespace')}) {
+
+    # And load them safely
+    my $e = $loader->load($module);
+    warn qq/Loading "$module" failed: $e/ if ref $e;
+  }
 
 =head1 DESCRIPTION
 

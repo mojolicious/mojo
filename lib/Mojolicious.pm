bdc81b4d45a3161ebb1f3007e8797ca95d3d36bc
Sebastian Riedel <sri@cpan.org>
2011-08-17 21:16:14 +0200

    improved cookbook recipe

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -34,7 +34,7 @@ has static   => sub { Mojolicious::Static->new };
 has types    => sub { Mojolicious::Types->new };
 
 our $CODENAME = 'Smiling Face With Sunglasses';
-our $VERSION  = '1.80';
+our $VERSION  = '1.81';
 
 # "These old doomsday devices are dangerously unstable.
 #  I'll rest easier not knowing where they are."

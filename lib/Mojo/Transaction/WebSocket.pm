5a87100d285e155760cfc22da4535aabc56dbe61
Sebastian Riedel <sri@cpan.org>
2011-10-16 19:12:30 +0200

    we already support IETF-17 WebSockets

--- lib/Mojo/Transaction/WebSocket.pm
+++ lib/Mojo/Transaction/WebSocket.pm
@@ -361,7 +361,7 @@ Mojo::Transaction::WebSocket - WebSocket transaction container
 
 L<Mojo::Transaction::WebSocket> is a container for WebSocket transactions as
 described in
-L<http://www.ietf.org/id/draft-ietf-hybi-thewebsocketprotocol-16.txt>.
+L<http://www.ietf.org/id/draft-ietf-hybi-thewebsocketprotocol-17.txt>.
 Note that this module is EXPERIMENTAL and might change without warning!
 
 =head1 EVENTS

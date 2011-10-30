55902189c75e0b70542f63ebeff451e3de0d43cb
Sebastian Riedel <sri@cpan.org>
2011-10-30 22:50:28 +0100

    mention cheatsheet in routing guide

--- lib/Mojolicious/Lite.pm
+++ lib/Mojolicious/Lite.pm
@@ -676,9 +676,9 @@ C<250KB> will be automatically streamed into a temporary file.
     </body>
   </html>
 
-To protect you from excessively large files there is also a global limit of
-C<5MB> by default, which you can tweak with the C<MOJO_MAX_MESSAGE_SIZE>
-environment variable.
+To protect you from excessively large files there is also a limit of C<5MB>
+by default, which you can tweak with the C<MOJO_MAX_MESSAGE_SIZE> environment
+variable.
 
   # Increase limit to 1GB
   $ENV{MOJO_MAX_MESSAGE_SIZE} = 1073741824;

5dbd9b0a58e2297684afc1f6c0e8332bbc508276
Marcus Ramberg <marcus@nordaaker.com>
2011-12-12 19:42:13 +0100

    Fix typos

--- lib/Mojo/IOLoop.pm
+++ lib/Mojo/IOLoop.pm
@@ -757,7 +757,7 @@ and the loop can be restarted by running C<start> again.
 Get L<Mojo::IOLoop::Stream> object for id or turn object into a connection.
 Note that this method is EXPERIMENTAL and might change without warning!
 
-  # Increase timeout for a transaction to 300 seconds
+  # Increase timeout for the connection of a transaction to 300 seconds
   Mojo::IOLoop->stream($tx->connection)->timeout(300);
 
 =head2 C<timer>

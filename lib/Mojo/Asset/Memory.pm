7ce0d96932f50023364470dd759d83c30db96c63
Brian Duggan <bduggan@matatu.org>
2012-01-23 10:42:52 -0500

    croak on syswrite errors

--- lib/Mojo/Asset/Memory.pm
+++ lib/Mojo/Asset/Memory.pm
@@ -54,7 +54,8 @@ sub move_to {
   my ($self, $path) = @_;
   croak qq/Can't open file "$path": $!/
     unless my $file = IO::File->new("> $path");
-  $file->syswrite($self->{content});
+  $file->syswrite($self->{content})
+     // croak qq/Can't move memory asset to "$path": $!/;
   return $self;
 }
 

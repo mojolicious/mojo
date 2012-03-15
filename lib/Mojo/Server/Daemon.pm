5a17f3934cafffdd31698fd0a1e4e52420875c7e
Sebastian Riedel <sri@cpan.org>
2012-03-15 16:50:47 +0100

    fixed IPv6 listen value bug in Mojo::Server::Daemon

--- lib/Mojo/Server/Daemon.pm
+++ lib/Mojo/Server/Daemon.pm
@@ -195,7 +195,7 @@ sub _listen {
 
   # DEPRECATED in Leaf Fluttering In Wind!
   my ($address, $port, $cert, $key, $ca);
-  if ($listen =~ qr|//(.+)\:(\d+)\:(.*?)\:(.*?)(?:\:(.+)?)?$|) {
+  if ($listen =~ qr|//([^\[\]]]+)\:(\d+)\:(.*?)\:(.*?)(?:\:(.+)?)?$|) {
     warn "Custom HTTPS listen values are DEPRECATED in favor of URLs!\n";
     ($address, $port, $cert, $key, $ca) = ($1, $2, $3, $4, $5);
   }

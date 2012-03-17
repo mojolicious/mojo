390b4f9b6598e0e40fc8e711d86c1ad8d7f19c7a
Sebastian Riedel <sri@cpan.org>
2012-03-17 02:54:00 +0100

    fixed inflate command class precedence

--- lib/Mojolicious/Command/inflate.pm
+++ lib/Mojolicious/Command/inflate.pm
@@ -14,7 +14,7 @@ sub run {
   my $all = {};
   my $app = $self->app;
   for my $class (@{$app->renderer->classes}, @{$app->static->classes}) {
-    $all = {%$all, %{$self->get_all_data($class)}};
+    $all = {%{$self->get_all_data($class)}, %$all};
   }
 
   # Turn them into real files

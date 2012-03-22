b1abaaf40153969e45a17d499cbff4f1844cdecb
Sebastian Riedel <sri@cpan.org>
2012-03-22 19:39:53 +0100

    added redirect to upload example

--- lib/Mojolicious/Lite.pm
+++ lib/Mojolicious/Lite.pm
@@ -674,11 +674,11 @@ into a temporary file.
       if $self->req->is_limit_exceeded;
 
     # Process uploaded file
-    if (my $example = $self->param('example')) {
-      my $size = $example->size;
-      my $name = $example->filename;
-      $self->render(text => "Thanks for uploading $size byte file $name.");
-    }
+    return $self->redirect_to('form')
+      unless my $example = $self->param('example');
+    my $size = $example->size;
+    my $name = $example->filename;
+    $self->render(text => "Thanks for uploading $size byte file $name.");
   };
 
   app->start;

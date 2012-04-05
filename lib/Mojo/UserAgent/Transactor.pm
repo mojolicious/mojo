fbf2ddb4edfa5723f12db27323f7f2827f86efc9
Sebastian Riedel <sri@cpan.org>
2012-04-05 05:57:44 +0200

    better form example

--- lib/Mojo/UserAgent/Transactor.pm
+++ lib/Mojo/UserAgent/Transactor.pm
@@ -279,9 +279,8 @@ data.
   # Inspect generated request
   say $t->form('mojolicio.us' => {a => [1, 2, 3]})->req->to_string;
 
-  # Submit form and stream response
-  my $tx = $t->form('http://kraih.com/foo' => {a => 'b'});
-  $tx->res->body(sub { say $_[1] });
+  # Streaming multipart file upload
+  my $tx = $t->form('mojolicio.us' => {fun => {file => '/etc/passwd'}});
   $ua->start($tx);
 
 While the "multipart/form-data" content type will be automatically used

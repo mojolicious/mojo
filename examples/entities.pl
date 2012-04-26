5649450d56e18649c7bfbedb1c05289d72ca1f86
Sebastian Riedel <sri@cpan.org>
2012-04-26 19:06:30 +0200

    better entity extraction script

--- examples/entities.pl
+++ examples/entities.pl
@@ -3,11 +3,13 @@ use lib "$FindBin::Bin/../lib";
 use Mojo::Base -strict;
 
 # "She's built like a steakhouse, but she handles like a bistro!"
+use Mojo::ByteStream 'b';
 use Mojo::UserAgent;
 
 # Extract named character references from HTML5 spec
-Mojo::UserAgent->new->get('http://dev.w3.org/html5/spec/single-page.html')
-  ->res->dom("#named-character-references-table tbody > tr")
-  ->each(sub { say $_->at("td > code")->text, $_->children("td")->[1]->text });
+my $tx
+  = Mojo::UserAgent->new->get('http://dev.w3.org/html5/spec/single-page.html');
+b($_->at('td > code')->text . $_->children('td')->[1]->text)->trim->say
+  for $tx->res->dom('#named-character-references-table tbody > tr')->each;
 
 1;

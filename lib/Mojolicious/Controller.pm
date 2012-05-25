be3796495459ba719c1842a424a5ca96034c6d0d
Sebastian Riedel <sri@cpan.org>
2012-05-25 07:24:17 +0200

    fixed typo in stash description

--- lib/Mojolicious/Controller.pm
+++ lib/Mojolicious/Controller.pm
@@ -902,8 +902,8 @@ Cookies failing signature verification will be automatically discarded.
   $c        = $c->stash(foo => 'bar');
 
 Non persistent data storage and exchange, application wide default values can
-be set with L<Mojolicious/"defaults">. Many stash value have a special meaning
-and are reserved, the full list is currently C<action>, C<app>, C<cb>,
+be set with L<Mojolicious/"defaults">. Many stash values have a special
+meaning and are reserved, the full list is currently C<action>, C<app>, C<cb>,
 C<controller>, C<data>, C<extends>, C<format>, C<handler>, C<json>, C<layout>,
 C<namespace>, C<partial>, C<path>, C<status>, C<template> and C<text>.
 

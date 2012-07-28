8d012abc1ea17da2594d7de9017625edb41de8e2
Sebastian Riedel <sri@cpan.org>
2012-07-28 13:43:59 +0200

    we now recommend Perl 5.14

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -34,7 +34,7 @@ has static   => sub { Mojolicious::Static->new };
 has types    => sub { Mojolicious::Types->new };
 
 our $CODENAME = 'Rainbow';
-our $VERSION  = '3.15';
+our $VERSION  = '3.16';
 
 # "These old doomsday devices are dangerously unstable.
 #  I'll rest easier not knowing where they are."

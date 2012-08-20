55ffb3abc00e735bffb31766a5bafaf73a7cc49e
Abhijit Menon-Sen <ams@toroid.org>
2012-08-20 18:07:49 +0530

    Bump VERSION after release

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -38,7 +38,7 @@ has static   => sub { Mojolicious::Static->new };
 has types    => sub { Mojolicious::Types->new };
 
 our $CODENAME = 'Rainbow';
-our $VERSION  = '3.32';
+our $VERSION  = '3.33';
 
 # "These old doomsday devices are dangerously unstable.
 #  I'll rest easier not knowing where they are."

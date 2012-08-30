c32d0fd0c90d160e4302a16f0b5c9ce45477b02c
Sebastian Riedel <sri@cpan.org>
2012-08-31 00:17:34 +0200

    updated jQuery to version 1.8.1

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -38,7 +38,7 @@ has static   => sub { Mojolicious::Static->new };
 has types    => sub { Mojolicious::Types->new };
 
 our $CODENAME = 'Rainbow';
-our $VERSION  = '3.36';
+our $VERSION  = '3.37';
 
 sub AUTOLOAD {
   my $self = shift;

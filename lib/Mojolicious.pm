0387018a530ab94c5bf89dcba63a00b3ed8f39d8
Sebastian Riedel <sri@cpan.org>
2012-09-25 00:24:44 +0200

    updated Changes file

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -38,7 +38,7 @@ has static   => sub { Mojolicious::Static->new };
 has types    => sub { Mojolicious::Types->new };
 
 our $CODENAME = 'Rainbow';
-our $VERSION  = '3.43';
+our $VERSION  = '3.44';
 
 sub AUTOLOAD {
   my $self = shift;

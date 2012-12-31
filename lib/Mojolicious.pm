4cf4fe87b0c2235f02de5f23fa420a986b834dc0
Sebastian Riedel <sri@cpan.org>
2012-12-31 12:21:44 +0100

    Happy New Year!

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -653,7 +653,7 @@ that have been bundled for internal use.
 
 =head2 Mojolicious Artwork
 
-  Copyright (C) 2010-2012, Sebastian Riedel.
+  Copyright (C) 2010-2013, Sebastian Riedel.
 
 Licensed under the CC-SA License, Version 3.0
 L<http://creativecommons.org/licenses/by-sa/3.0>.
@@ -940,7 +940,7 @@ Zak B. Elep
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2008-2012, Sebastian Riedel.
+Copyright (C) 2008-2013, Sebastian Riedel.
 
 This program is free software, you can redistribute it and/or modify it under
 the terms of the Artistic License version 2.0.

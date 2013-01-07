9c6510e2c4f9e26778dca2d14ef96357335d1357
Sebastian Riedel <sri@cpan.org>
2013-01-08 00:00:40 +0100

    added to_route method to Mojo::Path

--- lib/Mojolicious/Routes/Match.pm
+++ lib/Mojolicious/Routes/Match.pm
@@ -1,20 +1,15 @@
 package Mojolicious::Routes::Match;
 use Mojo::Base -base;
 
-use Mojo::Util qw(decode url_unescape);
-
 has captures => sub { {} };
 has [qw(endpoint root)];
 has stack => sub { [] };
 
 sub new {
   my $self = shift->SUPER::new;
-
-  $self->{method} = uc shift;
-  my $path = url_unescape shift;
-  $self->{path} = decode('UTF-8', $path) // $path;
+  $self->{method}    = uc shift;
+  $self->{path}      = shift;
   $self->{websocket} = shift;
-
   return $self;
 }
 

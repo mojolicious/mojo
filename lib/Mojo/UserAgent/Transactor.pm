c8498ac5645bb140e15beb5d785a153dcfbb20f9
Sebastian Riedel <sri@cpan.org>
2013-01-10 03:43:39 +0100

    fixed small upgrade bug in Mojo::UserAgent::Transactor

--- lib/Mojo/UserAgent/Transactor.pm
+++ lib/Mojo/UserAgent/Transactor.pm
@@ -194,7 +194,9 @@ sub _multipart {
         }
 
         # Memory
-        else { $part->asset->add_chunk(delete $value->{content}) }
+        elsif (defined(my $content = delete $value->{content})) {
+          $part->asset(Mojo::Asset::Memory->new->add_chunk($content));
+        }
 
         # Filename and headers
         $filename = delete $value->{filename} || $name;
--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -40,7 +40,7 @@ has static   => sub { Mojolicious::Static->new };
 has types    => sub { Mojolicious::Types->new };
 
 our $CODENAME = 'Rainbow';
-our $VERSION  = '3.76';
+our $VERSION  = '3.77';
 
 sub AUTOLOAD {
   my $self = shift;

mojolicious/mojo

commit 4f5bbde2e9802c8a3a156bc9546eba3992fb98b6

small optimization

lib/Mojo/Asset/File.pm
@@ -5,7 +5,7 @@ use Carp 'croak';
 use Errno 'EEXIST';
 use Fcntl qw(O_CREAT O_EXCL O_RDWR);
 use File::Copy 'move';
-use File::Spec;
+use File::Spec::Functions 'catfile';
 use IO::File;
 use Mojo::Util 'md5_sum';
 
@@ -22,7 +22,7 @@ has handle => sub {
   }
 
   # Open new or temporary file
-  my $base = File::Spec->catfile($self->tmpdir, 'mojo.tmp');
+  my $base = catfile File::Spec::Functions::tmpdir, 'mojo.tmp';
   my $name = $path // $base;
   until ($handle->open($name, O_CREAT | O_EXCL | O_RDWR)) {
     croak qq{Can't open file "$name": $!} if defined $path || $! != $!{EEXIST};
@@ -35,7 +35,7 @@ has handle => sub {
 
   return $handle;
 };
-has tmpdir => sub { $ENV{MOJO_TMPDIR} || File::Spec->tmpdir };
+has tmpdir => sub { $ENV{MOJO_TMPDIR} || File::Spec::Functions::tmpdir };
 
 sub DESTROY {
   my $self = shift;

commit 20036537f301469ef3603bab4b9426afcf371a8c

fixed regression bug from commit 4f5bbde2

lib/Mojo/Asset/File.pm
@@ -22,7 +22,7 @@ has handle => sub {
   }
 
   # Open new or temporary file
-  my $base = catfile File::Spec::Functions::tmpdir, 'mojo.tmp';
+  my $base = catfile $self->tmpdir, 'mojo.tmp';
   my $name = $path // $base;
   until ($handle->open($name, O_CREAT | O_EXCL | O_RDWR)) {
     croak qq{Can't open file "$name": $!} if defined $path || $! != $!{EEXIST};

t/mojo/asset.t
@@ -1,5 +1,6 @@
 use Mojo::Base -strict;
 
+use File::Basename 'dirname';
 use Test::More;
 use Mojo::Asset::File;
 use Mojo::Asset::Memory;
@@ -169,7 +170,16 @@ ok !$asset->is_file, 'stored in memory';
 # Temporary directory
 {
   local $ENV{MOJO_TMPDIR} = '/does/not/exist';
-  is(Mojo::Asset::File->new->tmpdir, '/does/not/exist', 'right value');
+  $asset = Mojo::Asset::File->new;
+  is($asset->tmpdir, '/does/not/exist', 'right value');
+  my $h = eval { $asset->handle };
+  if ($h) {
+    # in case /does/not/exist actually exists
+    is dirname($asset->path), '/does/not/exist', 'right tmpdir';
+  }
+  else {
+    like $@, qr(^Can't open file "/does/not/exist/mojo.tmp"), 'right tmpdir';
+  }
 }
 
 # Temporary file without cleanup
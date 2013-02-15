75d27a4d0da107fd852c44f2ed269f9cf7a9af7a
Sebastian Riedel <sri@cpan.org>
2013-02-15 01:20:48 +0100

    fixed a few typos

--- lib/Mojo/UserAgent/Transactor.pm
+++ lib/Mojo/UserAgent/Transactor.pm
@@ -289,10 +289,10 @@ Mojo::UserAgent::Transactor - User agent transactor
   say $t->tx(PATCH => 'mojolicio.us' => {DNT => 1} => 'Hi!')->req->to_string;
 
   # POST request with form data
-  say $t->tx(POST => 'http://kraih.com' => form => {a => 'b')->req->to_string;
+  say $t->tx(POST => 'kraih.com' => form => {a => 'b'})->req->to_string;
 
   # PUT request with JSON data
-  say $t->tx(PUT => 'http://kraih.com' => json => {a => 'b')->req->to_string;
+  say $t->tx(PUT => 'http://kraih.com' => json => {a => 'b'})->req->to_string;
 
 =head1 DESCRIPTION
 

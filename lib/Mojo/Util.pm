eebfd1518e0d42b684ef33b04103169dbea768e9
Marcus Ramberg <marcus@nordaaker.com>
2013-02-23 15:31:31 +0100

    Fix a couple of missed deprecations

--- lib/Mojo/Util.pm
+++ lib/Mojo/Util.pm
@@ -123,10 +123,8 @@ sub hmac_sha1_sum { _hmac(\&sha1, @_) }
 
 # DEPRECATED in Rainbow!
 sub html_escape {
-  warn <<EOF;
-Mojo::Util->html_escape is DEPRECATED in favor of Mojo::Util->xml_escape!!!
-EOF
-  my ($string, $pattern) = @_;
+  deprecated "Mojo::Util->html_escape is DEPRECATED "
+    . "in favor of Mojo::Util->xml_escape!!!" my ($string, $pattern) = @_;
   $pattern ||= '^\n\r\t !#$%(-;=?-~';
   return $string unless $string =~ /[^$pattern]/;
   $string =~ s/([$pattern])/_encode($1)/ge;

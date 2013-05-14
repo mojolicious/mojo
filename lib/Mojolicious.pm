ca9bfbfdd6740062fff3399492343d717685ac82
Sebastian Riedel <sri@cpan.org>
2013-05-14 18:21:52 +0200

    hide render_maybe

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -70,9 +70,10 @@ sub new {
 
   # Hide controller attributes/methods and "handler"
   $r->hide(qw(AUTOLOAD DESTROY app cookie finish flash handler on param));
-  $r->hide(qw(redirect_to render render_exception render_not_found));
-  $r->hide(qw(render_static rendered req res respond_to send session));
-  $r->hide(qw(signed_cookie stash tx ua url_for write write_chunk));
+  $r->hide(qw(redirect_to render render_exception render_maybe));
+  $r->hide(qw(render_not_found render_static rendered req res respond_to));
+  $r->hide(qw(send session signed_cookie stash tx ua url_for write));
+  $r->hide('write_chunk');
 
   # Check if we have a log directory
   my $mode = $self->mode;

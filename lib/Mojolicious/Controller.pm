5a6f74f94547b53f5c0ee5f17ffcf72b7caaa7c5
Sebastian Riedel <sri@cpan.org>
2013-06-08 06:27:33 +0200

    do not capture scheme

--- lib/Mojolicious/Controller.pm
+++ lib/Mojolicious/Controller.pm
@@ -382,7 +382,7 @@ sub url_for {
 
   # Absolute URL
   return $target if Scalar::Util::blessed $target && $target->isa('Mojo::URL');
-  return Mojo::URL->new($target) if $target =~ m!^(?:([^:/?#]+:)|//)!;
+  return Mojo::URL->new($target) if $target =~ m!^(?:[^:/?#]+:|//)!;
 
   # Base
   my $url  = Mojo::URL->new;

319c030f4ec54804a64657391614c07556f5b1fa
Adam Menges <adam.menges@sendgrid.com>
2013-08-20 11:50:10 -0600

    Support for more idiomatic code

--- lib/Mojo/DOM/HTML.pm
+++ lib/Mojo/DOM/HTML.pm
@@ -78,9 +78,7 @@ my %INLINE = map { $_ => 1 } (
 sub parse {
   my ($self, $html) = @_;
 
-  if (!(defined $html)) {
-    $html = "";
-  }
+  $html //= '';
 
   my $current = my $tree = ['root'];
   while ($html =~ m/\G$TOKEN_RE/gcs) {

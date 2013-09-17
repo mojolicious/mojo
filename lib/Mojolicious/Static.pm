8e7f13d9f83dae4bc4a7d866138fe0f003a37a1a
Sebastian Riedel <sri@cpan.org>
2013-09-17 15:59:40 +0200

    mention which methods do not protect from traversing to parent directories

--- lib/Mojolicious/Static.pm
+++ lib/Mojolicious/Static.pm
@@ -178,18 +178,24 @@ Serve static file for L<Mojolicious::Controller> object.
 
 =head2 file
 
-  my $asset = $static->file('foo/bar.html');
+  my $asset = $static->file('images/logo.png');
+  my $asset = $static->file('../lib/MyApp.pm');
 
 Get L<Mojo::Asset::File> or L<Mojo::Asset::Memory> object for a file, relative
-to C<paths> or from C<classes>.
+to C<paths> or from C<classes>. Note that this method does not protect from
+traversing to parent directories.
 
   my $content = $static->file('foo/bar.html')->slurp;
 
 =head2 serve
 
-  my $success = $static->serve(Mojolicious::Controller->new, 'foo/bar.html');
+  my $success =
+    $static->serve(Mojolicious::Controller->new, 'images/logo.png');
+  my $success =
+    $static->serve(Mojolicious::Controller->new, '../lib/MyApp.pm');
 
-Serve a specific file, relative to C<paths> or from C<classes>.
+Serve a specific file, relative to C<paths> or from C<classes>. Note that this
+method does not protect from traversing to parent directories.
 
 =head2 serve_asset
 

536f6adbca437c8ab7812e0e6ccf9434802f6550
Sebastian Riedel <sri@cpan.org>
2013-09-20 23:30:20 +0200

    mention text method in Mojolicious::Controller

--- lib/Mojolicious/Controller.pm
+++ lib/Mojolicious/Controller.pm
@@ -735,6 +735,7 @@ Get L<Mojo::Message::Request> object from L<Mojo::Transaction/"req">.
   my $host     = $c->req->url->to_abs->host;
   my $agent    = $c->req->headers->user_agent;
   my $body     = $c->req->body;
+  my $str      = $c->req->text;
   my $hash     = $c->req->json;
   my $foo      = $c->req->json('/23/foo');
   my $dom      = $c->req->dom;

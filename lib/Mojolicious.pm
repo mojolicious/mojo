b2484c2f45fd1084d1840dd2a07c2172fa1e5702
Sebastian Riedel <sri@cpan.org>
2014-01-29 22:44:47 +0100

    fixed typo in rendering guide

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -43,7 +43,7 @@ has types     => sub { Mojolicious::Types->new };
 has validator => sub { Mojolicious::Validator->new };
 
 our $CODENAME = 'Top Hat';
-our $VERSION  = '4.72';
+our $VERSION  = '4.73';
 
 sub AUTOLOAD {
   my $self = shift;

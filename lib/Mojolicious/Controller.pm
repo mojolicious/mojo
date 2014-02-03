0526aabc75fb1b06a1413a047530d9487e676881
Sebastian Riedel <sri@cpan.org>
2014-02-03 07:21:31 +0100

    added template rendering examples

--- lib/Mojolicious/Controller.pm
+++ lib/Mojolicious/Controller.pm
@@ -697,6 +697,12 @@ the L</"stash">.
   # Render JSON
   $c->render(json => {test => 'I ♥ Mojolicious!'});
 
+  # Render template "foo/bar.html.ep"
+  $c->render('foo/bar', format => 'html', handler => 'ep');
+
+  # Render template "foo/bar.*.*"
+  $c->render('foo/bar');
+
 =head2 render_exception
 
   $c = $c->render_exception('Oops!');

e008f411f2e47fb8c82a46c6293d2fe77fedb118
Andrey Gatitskiy <gatitskiy@gmail.com>
2014-03-04 13:30:21 +0300

    Able to pass the password for key file

--- lib/Mojo/IOLoop/Client.pm
+++ lib/Mojo/IOLoop/Client.pm
@@ -112,7 +112,8 @@ sub _try {
     SSL_startHandshake  => 0,
     SSL_verify_mode     => $args->{tls_ca} ? 0x01 : 0x00,
     SSL_verifycn_name   => $args->{address},
-    SSL_verifycn_scheme => $args->{tls_ca} ? 'http' : undef
+    SSL_verifycn_scheme => $args->{tls_ca} ? 'http' : undef,
+    SSL_passwd_cb       => sub { $args->{password} }
   );
   my $reactor = $self->reactor;
   $reactor->remove($handle);

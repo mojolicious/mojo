ae504b714c7676be40cd737f0a41d14de203a72c
Sebastian Riedel <sri@cpan.org>
2014-03-04 06:41:09 +0100

    do not use Exporter as a base class

--- lib/Mojo/Util.pm
+++ lib/Mojo/Util.pm
@@ -1,11 +1,12 @@
 package Mojo::Util;
-use Mojo::Base 'Exporter';
+use Mojo::Base -strict;
 
 use Carp qw(carp croak);
 use Data::Dumper ();
 use Digest::MD5 qw(md5 md5_hex);
 use Digest::SHA qw(hmac_sha1_hex sha1 sha1_hex);
 use Encode 'find_encoding';
+use Exporter 'import';
 use File::Basename 'dirname';
 use File::Spec::Functions 'catfile';
 use List::Util 'min';

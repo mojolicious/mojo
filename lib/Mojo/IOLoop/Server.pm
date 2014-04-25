5e240457b74c39b2fca96318888d5e9758c92fe9
Sebastian Riedel <sri@cpan.org>
2014-04-25 03:22:47 +0200

    improved Mojo::IOLoop::Server to reuse cipher list from IO::Socket::SSL

--- lib/Mojo/IOLoop/Server.pm
+++ lib/Mojo/IOLoop/Server.pm
@@ -89,18 +89,16 @@ sub listen {
   return unless $args->{tls};
   croak "IO::Socket::SSL 1.84 required for TLS support" unless TLS;
 
-  # Prioritize RC4 to mitigate BEAST attack
-  $self->{tls} = {
-    SSL_ca_file => $args->{tls_ca}
-      && -T $args->{tls_ca} ? $args->{tls_ca} : undef,
+  my $tls = $self->{tls} = {
     SSL_cert_file => $args->{tls_cert} || $CERT,
-    SSL_cipher_list => $args->{tls_ciphers}
-      // 'ECDHE-RSA-AES128-SHA256:AES128-GCM-SHA256:RC4:HIGH:!MD5:!aNULL:!EDH',
     SSL_honor_cipher_order => 1,
     SSL_key_file           => $args->{tls_key} || $KEY,
     SSL_startHandshake     => 0,
     SSL_verify_mode => $args->{tls_verify} // $args->{tls_ca} ? 0x03 : 0x00
   };
+  $tls->{SSL_ca_file} = $args->{tls_ca}
+    if $args->{tls_ca} && -T $args->{tls_ca};
+  $tls->{SSL_cipher_list} = $args->{tls_ciphers} if $args->{tls_ciphers};
 }
 
 sub start {
@@ -288,8 +286,7 @@ Path to the TLS cert file, defaults to a built-in test certificate.
 
   tls_ciphers => 'AES128-GCM-SHA256:RC4:HIGH:!MD5:!aNULL:!EDH'
 
-Cipher specification string, defaults to
-C<ECDHE-RSA-AES128-SHA256:AES128-GCM-SHA256:RC4:HIGH:!MD5:!aNULL:!EDH>.
+Cipher specification string.
 
 =item tls_key
 
--- lib/Mojo/Server/Daemon.pm
+++ lib/Mojo/Server/Daemon.pm
@@ -373,8 +373,7 @@ Path to the TLS cert file, defaults to a built-in test certificate.
 
   ciphers=AES128-GCM-SHA256:RC4:HIGH:!MD5:!aNULL:!EDH
 
-Cipher specification string, defaults to
-C<ECDHE-RSA-AES128-SHA256:AES128-GCM-SHA256:RC4:HIGH:!MD5:!aNULL:!EDH>.
+Cipher specification string.
 
 =item key
 

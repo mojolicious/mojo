7d1d2d942962044113c4363968b0e29125265367
Sebastian Riedel <sri@cpan.org>
2014-04-30 00:12:57 +0200

    bump version

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -43,7 +43,7 @@ has types     => sub { Mojolicious::Types->new };
 has validator => sub { Mojolicious::Validator->new };
 
 our $CODENAME = 'Top Hat';
-our $VERSION  = '4.97';
+our $VERSION  = '4.98';
 
 sub AUTOLOAD {
   my $self = shift;

76842a46239babc4c5ec65333ab0b23d087f8199
Sebastian Riedel <sri@cpan.org>
2014-07-21 14:00:11 +0200

    use less syscalls for appending to assets

--- lib/Mojo/Asset/File.pm
+++ lib/Mojo/Asset/File.pm
@@ -3,7 +3,7 @@ use Mojo::Base 'Mojo::Asset';
 
 use Carp 'croak';
 use Errno 'EEXIST';
-use Fcntl qw(O_CREAT O_EXCL O_RDONLY O_RDWR);
+use Fcntl qw(O_APPEND O_CREAT O_EXCL O_RDONLY O_RDWR);
 use File::Copy 'move';
 use File::Spec::Functions 'catfile';
 use IO::File;
@@ -17,7 +17,7 @@ has handle => sub {
   my $handle = IO::File->new;
   my $path   = $self->path;
   if (defined $path && -f $path) {
-    $handle->open($path, -w _ ? O_RDWR : O_RDONLY)
+    $handle->open($path, -w _ ? O_APPEND | O_RDWR : O_RDONLY)
       or croak qq{Can't open file "$path": $!};
     return $handle;
   }
@@ -25,7 +25,7 @@ has handle => sub {
   # Open new or temporary file
   my $base = catfile $self->tmpdir, 'mojo.tmp';
   my $name = $path // $base;
-  until ($handle->open($name, O_CREAT | O_EXCL | O_RDWR)) {
+  until ($handle->open($name, O_APPEND | O_CREAT | O_EXCL | O_RDWR)) {
     croak qq{Can't open file "$name": $!} if defined $path || $! != $!{EEXIST};
     $name = "$base." . md5_sum(time . $$ . rand 999);
   }
@@ -47,13 +47,9 @@ sub DESTROY {
 
 sub add_chunk {
   my ($self, $chunk) = @_;
-
-  my $handle = $self->handle;
-  $handle->sysseek(0, SEEK_END);
   $chunk //= '';
   croak "Can't write to asset: $!"
-    unless defined $handle->syswrite($chunk, length $chunk);
-
+    unless defined $self->handle->syswrite($chunk, length $chunk);
   return $self;
 }
 

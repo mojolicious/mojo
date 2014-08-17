190e919d9f8bca0df02e377da6995022ce6fa555
Sebastian Riedel <sri@cpan.org>
2014-08-17 23:49:40 +0200

    bump version

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -43,7 +43,7 @@ has types     => sub { Mojolicious::Types->new };
 has validator => sub { Mojolicious::Validator->new };
 
 our $CODENAME = 'Tiger Face';
-our $VERSION  = '5.30';
+our $VERSION  = '5.31';
 
 sub AUTOLOAD {
   my $self = shift;

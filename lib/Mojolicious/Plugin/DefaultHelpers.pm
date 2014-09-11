e51f30f6648a7477fdfbb40003b0d8f756d01ac3
Sebastian Riedel <sri@cpan.org>
2014-09-12 00:13:14 +0200

    moved around a few sections in the guides

--- lib/Mojolicious/Plugin/DefaultHelpers.pm
+++ lib/Mojolicious/Plugin/DefaultHelpers.pm
@@ -162,7 +162,7 @@ Mojolicious::Plugin::DefaultHelpers - Default helpers plugin
 
 =head1 DESCRIPTION
 
-L<Mojolicious::Plugin::DefaultHelpers> is a collection of renderer helpers for
+L<Mojolicious::Plugin::DefaultHelpers> is a collection of helpers for
 L<Mojolicious>.
 
 This is a core plugin, that means it is always enabled and its code a good

8c79010710b4065bb9b42d7f9b58d9f0a7bffca5
Sebastian Riedel <sri@cpan.org>
2014-10-30 01:35:54 +0100

    simplify AUTOLOAD example

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -675,10 +675,10 @@ object, so they can't depend on or change controller state, which includes
 request, response and stash.
 
   # Call helper
-  $app->log->debug($app->dumper({foo => 'bar'}));
+  say $app->dumper({foo => 'bar'});
 
   # Longer version
-  $app->log->debug($app->build_controller->helpers->dumper({foo => 'bar'}));
+  say $app->build_controller->helpers->dumper({foo => 'bar'});
 
 =head1 BUNDLED FILES
 

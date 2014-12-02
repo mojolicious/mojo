6e9228a51e4b8b7d87d74b83a66c69ff03ea8147
Sebastian Riedel <sri@cpan.org>
2014-12-02 15:42:24 +0100

    fixed typo in description

--- lib/Mojolicious/Controller.pm
+++ lib/Mojolicious/Controller.pm
@@ -667,7 +667,7 @@ Prepare a C<302> redirect response, takes the same arguments as L</"url_for">.
 Render content with L<Mojolicious::Renderer/"render"> and emit hooks
 L<Mojolicious/"before_render"> as well as L<Mojolicious/"after_render">, or
 call L<Mojolicious::Plugin::DefaultHelpers/"reply-E<gt>not_found"> if no
-response could be generated,, all additional pairs get merged into the
+response could be generated, all additional pairs get merged into the
 L</"stash">.
 
   # Render characters

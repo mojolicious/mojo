e6360a88fdbd61cd0f0b1d4351b6b5257b6085c0
Sebastian Riedel <sri@cpan.org>
2015-02-21 07:02:48 +0100

    mention which files are used

--- lib/Mojolicious/Renderer.pm
+++ lib/Mojolicious/Renderer.pm
@@ -290,7 +290,8 @@ Renderer cache, defaults to a L<Mojo::Cache> object.
   $renderer   = $renderer->classes(['main']);
 
 Classes to use for finding templates in C<DATA> sections with L<Mojo::Loader>,
-first one has the highest precedence, defaults to C<main>.
+first one has the highest precedence, defaults to C<main>. Only files with
+exactly two extensions will be used, like C<index.html.ep>.
 
   # Add another class with templates in DATA section
   push @{$renderer->classes}, 'Mojolicious::Plugin::Fun';

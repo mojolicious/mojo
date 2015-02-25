9b758ca00ac11d08e4eef2d8558e27c9e0f20aad
Sebastian Riedel <sri@cpan.org>
2015-02-25 06:14:18 +0100

    shortcuts for all prefork options

--- lib/Mojolicious/Command/prefork.pm
+++ lib/Mojolicious/Command/prefork.pm
@@ -13,21 +13,21 @@ sub run {
 
   my $prefork = Mojo::Server::Prefork->new(app => $self->app);
   GetOptionsFromArray \@args,
-    'A|accepts=i'            => sub { $prefork->accepts($_[1]) },
+    'a|accepts=i'            => sub { $prefork->accepts($_[1]) },
     'b|backlog=i'            => sub { $prefork->backlog($_[1]) },
     'c|clients=i'            => sub { $prefork->max_clients($_[1]) },
     'G|graceful-timeout=i'   => sub { $prefork->graceful_timeout($_[1]) },
     'g|group=s'              => sub { $prefork->group($_[1]) },
-    'heartbeat-interval=i'   => sub { $prefork->heartbeat_interval($_[1]) },
+    'I|heartbeat-interval=i' => sub { $prefork->heartbeat_interval($_[1]) },
     'H|heartbeat-timeout=i'  => sub { $prefork->heartbeat_timeout($_[1]) },
     'i|inactivity-timeout=i' => sub { $prefork->inactivity_timeout($_[1]) },
-    'l|listen=s'     => \my @listen,
-    'multi-accept=i' => sub { $prefork->multi_accept($_[1]) },
-    'P|pid-file=s'   => sub { $prefork->pid_file($_[1]) },
-    'p|proxy'        => sub { $prefork->reverse_proxy(1) },
-    'r|requests=i'   => sub { $prefork->max_requests($_[1]) },
-    'u|user=s'       => sub { $prefork->user($_[1]) },
-    'w|workers=i'    => sub { $prefork->workers($_[1]) };
+    'l|listen=s'       => \my @listen,
+    'M|multi-accept=i' => sub { $prefork->multi_accept($_[1]) },
+    'P|pid-file=s'     => sub { $prefork->pid_file($_[1]) },
+    'p|proxy'          => sub { $prefork->reverse_proxy(1) },
+    'r|requests=i'     => sub { $prefork->max_requests($_[1]) },
+    'u|user=s'         => sub { $prefork->user($_[1]) },
+    'w|workers=i'      => sub { $prefork->workers($_[1]) };
 
   $prefork->listen(\@listen) if @listen;
   $prefork->run;
@@ -51,7 +51,7 @@ Mojolicious::Command::prefork - Prefork command
     ./myapp.pl prefork -l 'https://*:443?cert=./server.crt&key=./server.key'
 
   Options:
-    -A, --accepts <number>               Number of connections for workers to
+    -a, --accepts <number>               Number of connections for workers to
                                          accept, defaults to 1000
     -b, --backlog <size>                 Listen backlog size, defaults to
                                          SOMAXCONN
@@ -59,14 +59,14 @@ Mojolicious::Command::prefork - Prefork command
                                          connections, defaults to 1000
     -G, --graceful-timeout <seconds>     Graceful timeout, defaults to 20.
     -g, --group <name>                   Group name for process
-        --heartbeat-interval <seconds>   Heartbeat interval, defaults to 5
+    -I, --heartbeat-interval <seconds>   Heartbeat interval, defaults to 5
     -H, --heartbeat-timeout <seconds>    Heartbeat timeout, defaults to 20
     -i, --inactivity-timeout <seconds>   Inactivity timeout, defaults to the
                                          value of MOJO_INACTIVITY_TIMEOUT or 15
     -l, --listen <location>              One or more locations you want to
                                          listen on, defaults to the value of
                                          MOJO_LISTEN or "http://*:3000"
-        --multi-accept <number>          Number of connections to accept at
+    -M, --multi-accept <number>          Number of connections to accept at
                                          once, defaults to 50
     -P, --pid-file <path>                Path to process id file, defaults to a
                                          random file

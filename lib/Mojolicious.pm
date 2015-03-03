b829a00aabde6516759e620623227a99db3a4dd7
Sebastian Riedel <sri@cpan.org>
2015-03-03 17:11:40 +0100

    bump version

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -43,7 +43,7 @@ has types     => sub { Mojolicious::Types->new };
 has validator => sub { Mojolicious::Validator->new };
 
 our $CODENAME = 'Clinking Beer Mugs';
-our $VERSION  = '6.01';
+our $VERSION  = '6.02';
 
 sub AUTOLOAD {
   my $self = shift;

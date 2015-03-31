a6cef6b820c710aac2c36e7bb633fa24e1c1cb97
Sebastian Riedel <sri@cpan.org>
2015-03-31 05:35:45 +0200

    make entities example a little more readable

--- examples/entities.pl
+++ examples/entities.pl
@@ -1,10 +1,14 @@
 use Mojo::Base -strict;
-use Mojo::ByteStream 'b';
 use Mojo::UserAgent;
+use Mojo::Util 'trim';
 
-# Extract named character references from HTML spec
-my $tx = Mojo::UserAgent->new->get('https://html.spec.whatwg.org');
-b($_->at('td > code')->text . ' ' . $_->children('td')->[1]->text)->trim->say
-  for $tx->res->dom('#named-character-references-table tbody > tr')->each;
+# Extract named character references from HTML Living Standard
+my $tx   = Mojo::UserAgent->new->get('https://html.spec.whatwg.org');
+my $rows = $tx->res->dom('#named-character-references-table tbody > tr');
+for my $row ($rows->each) {
+  my $entity     = $row->at('td > code')->text;
+  my $codepoints = $row->children('td')->[1]->text;
+  say trim "$entity $codepoints";
+}
 
 1;

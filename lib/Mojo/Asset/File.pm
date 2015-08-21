b53ee1f63a80ae10ce2342dc349e07f7373727ef
Sebastian Riedel <sri@cpan.org>
2015-08-21 19:31:32 +0200

    a chunk is required

--- lib/Mojo/Asset/File.pm
+++ lib/Mojo/Asset/File.pm
@@ -46,8 +46,7 @@ sub DESTROY {
 
 sub add_chunk {
   my ($self, $chunk) = @_;
-  defined $self->handle->syswrite($chunk // '')
-    or croak "Can't write to asset: $!";
+  defined $self->handle->syswrite($chunk) or croak "Can't write to asset: $!";
   return $self;
 }
 

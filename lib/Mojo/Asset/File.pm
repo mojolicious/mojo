880f4a603fec23c7f4a6aa79884b27bb66015c7e
Sebastian Riedel <sri@cpan.org>
2016-01-27 17:31:15 +0100

    check for handle first

--- lib/Mojo/Asset/File.pm
+++ lib/Mojo/Asset/File.pm
@@ -40,7 +40,7 @@ has tmpdir => sub { $ENV{MOJO_TMPDIR} || File::Spec::Functions::tmpdir };
 sub DESTROY {
   my $self = shift;
   return unless $self->cleanup && defined(my $path = $self->path);
-  close $self->handle;
+  if (my $handle = $self->handle) { close $handle }
   unlink $path if -w $path;
 }
 

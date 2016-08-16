e519353a58ac26c96795e9cf5382e99bb9a964f5
Roy Storey <kiwiroy@users.noreply.github.com>
2016-08-16 22:04:24 +1200

    reset $. removes addendum

--- lib/Mojo/Loader.pm
+++ lib/Mojo/Loader.pm
@@ -84,7 +84,7 @@ sub _all {
     $all->{$name} = $name =~ s/\s*\(\s*base64\s*\)$//
       && ++$BIN{$class}{$name} ? b64_decode $data : $data;
   }
-
+  $. = 0;
   return $all;
 }
 
--- t/mojo/lib/Mojo/LoaderTest/C.pm
+++ t/mojo/lib/Mojo/LoaderTest/C.pm
@@ -3,3 +3,10 @@ package Mojo::LoaderTest::C;
 use Mojo::Base -base;
 
 1;
+__DATA__
+
+@@ basic.html
+<html>
+  <head><title></title></head>
+  <body>Hello World</body>
+</html>

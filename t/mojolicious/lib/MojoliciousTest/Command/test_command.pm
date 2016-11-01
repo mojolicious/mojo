1ee6074c69d529d7637a1cd4ce3ea9354ed7609b
Sebastian Riedel <sri@cpan.org>
2016-11-01 19:35:42 +0100

    use getopt everywhere

--- t/mojolicious/lib/MojoliciousTest/Command/test_command.pm
+++ t/mojolicious/lib/MojoliciousTest/Command/test_command.pm
@@ -1,11 +1,11 @@
 package MojoliciousTest::Command::test_command;
 use Mojo::Base 'Mojolicious::Command';
 
-use Getopt::Long 'GetOptionsFromArray';
+use Mojo::Util 'getopt';
 
 sub run {
   my ($self, @args) = @_;
-  GetOptionsFromArray \@args, 'too' => \my $too;
+  getopt \@args, ['default'], 'too' => \my $too;
   return $too ? 'works too!' : 'works!';
 }
 

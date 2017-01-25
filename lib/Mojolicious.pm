a1c045a57379124341d9b25de7d3129ee3c5e7d5
Sebastian Riedel <sri@cpan.org>
2017-01-26 00:10:03 +0100

    bump version

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -57,7 +57,7 @@ has types     => sub { Mojolicious::Types->new };
 has validator => sub { Mojolicious::Validator->new };
 
 our $CODENAME = 'Doughnut';
-our $VERSION  = '7.22';
+our $VERSION  = '7.23';
 
 sub AUTOLOAD {
   my $self = shift;

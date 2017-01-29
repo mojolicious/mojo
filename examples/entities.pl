66a00205c1aff15dba51ed315e08f907056b59b2
Sebastian Riedel <sri@cpan.org>
2017-01-29 23:51:00 +0100

    use current best practices in examples

--- examples/entities.pl
+++ examples/entities.pl
@@ -2,9 +2,8 @@ use Mojo::Base -strict;
 use Mojo::UserAgent;
 
 # Extract named character references from HTML Living Standard
-my $tx   = Mojo::UserAgent->new->get('https://html.spec.whatwg.org');
-my $rows = $tx->res->dom('#named-character-references-table tbody > tr');
-for my $row ($rows->each) {
+my $res = Mojo::UserAgent->new->get('https://html.spec.whatwg.org')->result;
+for my $row ($res->dom('#named-character-references-table tbody > tr')->each) {
   my $entity     = $row->at('td > code')->text;
   my $codepoints = $row->children('td')->[1]->text;
   say "$entity $codepoints";

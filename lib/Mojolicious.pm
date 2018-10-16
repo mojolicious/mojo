ff84acdbcc2174614f1c00e689abf18c5e342817
Sebastian Riedel <sri@cpan.org>
2018-10-17 00:42:15 +0200

    bump version

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -58,7 +58,7 @@ has ua        => sub { Mojo::UserAgent->new };
 has validator => sub { Mojolicious::Validator->new };
 
 our $CODENAME = 'Supervillain';
-our $VERSION  = '8.03';
+our $VERSION  = '8.04';
 
 sub AUTOLOAD {
   my $self = shift;

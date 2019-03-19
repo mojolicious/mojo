9c8bbd734ed4322cec19a8b39b1f2dbfddc57629
Dabrien 'Dabe' Murphy <dabe@parabon.com>
2019-03-19 14:23:13 -0400

    fix: Silence "ambiguous scalar" Warnings

--- lib/Mojo/Template.pm
+++ lib/Mojo/Template.pm
@@ -213,11 +213,11 @@ sub _compile {
 
       # Escaped
       if (!$multi && ($op eq 'escp' && !$escape || $op eq 'expr' && $escape)) {
-        $blocks[-1] .= "\$_O .= _escape scalar + $value";
+        $blocks[-1] .= "no warnings 'ambiguous'; \$_O .= _escape scalar + $value";
       }
 
       # Raw
-      elsif (!$multi) { $blocks[-1] .= "\$_O .= scalar + $value" }
+      elsif (!$multi) { $blocks[-1] .= "no warnings 'ambiguous'; \$_O .= scalar + $value" }
 
       # Multi-line
       $multi = !$next || $next->[0] ne 'text';

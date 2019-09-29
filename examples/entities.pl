c3154de6c94a1378b65db7bd3634248378aa1b11
Sebastian Riedel <sri@cpan.org>
2019-09-29 18:08:28 +0200

    Entities need to be trimmed now

--- examples/entities.pl
+++ examples/entities.pl
@@ -2,12 +2,14 @@
 # Extract named character references from HTML Living Standard
 #
 use Mojo::Base -strict;
+
 use Mojo::UserAgent;
+use Mojo::Util 'trim';
 
 my $res = Mojo::UserAgent->new->get('https://html.spec.whatwg.org')->result;
 for my $row ($res->dom('#named-character-references-table tbody > tr')->each) {
-  my $entity     = $row->at('td > code')->text;
-  my $codepoints = $row->children('td')->[1]->text;
+  my $entity     = trim $row->at('td > code')->text;
+  my $codepoints = trim $row->children('td')->[1]->text;
   say "$entity $codepoints";
 }
 

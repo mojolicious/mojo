3481dea20d1e052820462b0aa1f09a052b84cce2
Sebastian Riedel <sri@cpan.org>
2020-02-27 11:28:43 +0100

    Use an array in the validation example to make it easier to distinguish the values

--- lib/Mojolicious/Validator/Validation.pm
+++ lib/Mojolicious/Validator/Validation.pm
@@ -231,7 +231,7 @@ the current L</"topic">.
 =head2 optional
 
   $v = $v->optional('foo');
-  $v = $v->optional('foo', 'filter1', 'filter2');
+  $v = $v->optional('foo', @filters);
 
 Change validation L</"topic"> and apply filters. All filters from
 L<Mojolicious::Validator/"FILTERS"> are supported.
@@ -263,7 +263,7 @@ Return an array reference with all names for values that passed validation.
 =head2 required
 
   $v = $v->required('foo');
-  $v = $v->required('foo', 'filter1', 'filter2');
+  $v = $v->required('foo', @filters);
 
 Change validation L</"topic">, apply filters, and make sure a value is present
 and not an empty string. All filters from L<Mojolicious::Validator/"FILTERS">

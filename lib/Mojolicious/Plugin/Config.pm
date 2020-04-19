776841a2997271d22662f852ed9e0d089b1c881b
Hauke D <haukex@zero-g.net>
2020-04-19 09:07:59 +0200

    Cleaned up with perltidy

--- lib/Mojolicious/Plugin/Config.pm
+++ lib/Mojolicious/Plugin/Config.pm
@@ -10,14 +10,24 @@ sub parse {
   my ($self, $content, $file, $conf, $app) = @_;
 
   # add the "#line" directive *only* if it's safe to do so
-  my $line_directive = $file=~/[\r\n]/ ? '' # can't break "#line" across lines
-    : ( $file=~/\s/ ? ( $file=~/"/ ? ''     # can't escape double-quotes
-      : qq{\n#line 1 "$file"\n} ) : qq{\n#line 1 $file\n} );
+  my $line_directive = $file =~ /[\r\n]/
+    ? ''    # can't break "#line" across lines
+    : (
+    $file =~ /\s/
+    ? (
+      $file =~ /"/
+      ? ''    # can't escape double-quotes
+      : qq{\n#line 1 "$file"\n}
+      )
+    : qq{\n#line 1 $file\n}
+    );
 
   # Run Perl code in sandbox
-  my $config = eval 'package Mojolicious::Plugin::Config::Sandbox; no warnings;'
+  my $config
+    = eval 'package Mojolicious::Plugin::Config::Sandbox; no warnings;'
     . "sub app; local *app = sub { \$app }; use Mojo::Base -strict;"
-    . $line_directive . $content;
+    . $line_directive
+    . $content;
   die qq{Can't load configuration from file "$file": $@} if $@;
   die qq{Configuration file "$file" did not return a hash reference.\n}
     unless ref $config eq 'HASH';
--- t/mojolicious/perl_config_lite_app.t
+++ t/mojolicious/perl_config_lite_app.t
@@ -5,11 +5,13 @@ use Mojo::File qw(curfile);
 use Mojolicious::Lite;
 
 plugin 'Config';
-is_deeply app->config, {
-    foo => "bar",
-    utf => "утф",
-    file => curfile->sibling('perl_config_lite_app.conf')->to_abs->to_string,
-    line => 7,
-  }, 'right value';
+is_deeply app->config,
+  {
+  foo  => "bar",
+  utf  => "утф",
+  file => curfile->sibling('perl_config_lite_app.conf')->to_abs->to_string,
+  line => 7,
+  },
+  'right value';
 
 done_testing();

f296e6363f50e67c17df1d2ab5adae49cc08abe0
Sebastian Riedel <sri@cpan.org>
2020-06-03 22:01:43 +0200

    Bump version

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -58,7 +58,7 @@ has ua        => sub { Mojo::UserAgent->new };
 has validator => sub { Mojolicious::Validator->new };
 
 our $CODENAME = 'Supervillain';
-our $VERSION  = '8.52';
+our $VERSION  = '8.53';
 
 sub BUILD_DYNAMIC {
   my ($class, $method, $dyn_methods) = @_;

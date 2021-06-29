487aaad9e67c377779420562e7f5cf3a4a9be118
Sebastian Riedel <sri@cpan.org>
2021-06-29 12:13:41 +0200

    We are boycotting The Perl Foundation

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -817,6 +817,8 @@ Current voting members of the core team in alphabetical order:
 
 =over 2
 
+CandyAngel, C<candyangel@mojolicious.org>
+
 Christopher Rasch-Olsen Raa, C<christopher@mojolicious.org>
 
 Dan Book, C<grinnz@mojolicious.org>
@@ -834,8 +836,6 @@ The following members of the core team are currently on hiatus:
 =over 2
 
 Abhijit Menon-Sen, C<ams@cpan.org>
-
-CandyAngel, C<candyangel@mojolicious.org>
 
 Glen Hinkle, C<tempire@cpan.org>
 

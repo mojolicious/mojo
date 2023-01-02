cfa34e0d320fd7bc78097dc4fda60abf6cf66cb0
Sebastian Riedel <s@kraih.com>
2023-01-02 18:11:44 +0100

    Happy new year

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -752,7 +752,7 @@ The L<Mojolicious> distribution includes a few files with different licenses tha
 
 =head2 Mojolicious Artwork
 
-  Copyright (C) 2010-2022, Sebastian Riedel.
+  Copyright (C) 2010-2023, Sebastian Riedel.
 
 Licensed under the CC-SA License, Version 4.0 L<http://creativecommons.org/licenses/by-sa/4.0>.
 
@@ -1192,7 +1192,7 @@ Zoffix Znet
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2008-2022, Sebastian Riedel and others.
+Copyright (C) 2008-2023, Sebastian Riedel and others.
 
 This program is free software, you can redistribute it and/or modify it under the terms of the Artistic License version
 2.0.

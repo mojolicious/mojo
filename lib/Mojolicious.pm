0555008e94c98b8546dd756d224dc5d2456d05dc
Sebastian Riedel <s@kraih.com>
2024-02-11 13:58:23 +0100

    Update Changes

--- lib/Mojolicious.pm
+++ lib/Mojolicious.pm
@@ -756,7 +756,7 @@ The L<Mojolicious> distribution includes a few files with different licenses tha
 
 =head2 Mojolicious Artwork
 
-  Copyright (C) 2010-2023, Sebastian Riedel.
+  Copyright (C) 2010-2024, Sebastian Riedel.
 
 Licensed under the CC-SA License, Version 4.0 L<http://creativecommons.org/licenses/by-sa/4.0>.
 
@@ -991,6 +991,8 @@ Heiko Jansen
 
 Henry Tang
 
+Hernan Lopes
+
 Hideki Yamamura
 
 Hiroki Toyokawa
@@ -1205,7 +1207,7 @@ Zoffix Znet
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2008-2023, Sebastian Riedel and others.
+Copyright (C) 2008-2024, Sebastian Riedel and others.
 
 This program is free software, you can redistribute it and/or modify it under the terms of the Artistic License version
 2.0.
